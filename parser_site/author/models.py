4354c3c38b5929d4dd8e50505a47d6a654caddeb
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-04-07 17:40:45 +0700

    create publication model

--- parser_site/author/models.py
+++ parser_site/author/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Publication(models.Model):
+    title = models.CharField(max_length=500)
+    authors = models.CharField(max_length=500)
+    info = models.CharField(max_length=500)
+    link = models.CharField(max_length=500)
+    year = models.CharField(max_length=500)
+# Создать класс Автор с соответствующими атрибутами (название, авторы, инфо, ссылки, год )?
+# в урлах ссылка на id автора, а views функция для парсинга
