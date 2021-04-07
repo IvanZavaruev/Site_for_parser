5a5798c793e9bf8e335a95d75ae2047c26d8ed96
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-04-07 17:43:01 +0700

    add parser

--- parser_site/author/views.py
+++ parser_site/author/views.py
@@ -1,8 +1,17 @@
+import tempfile
 from django.shortcuts import render
 from django.http import HttpResponse
+from elibrary_parser.Parsers import AuthorParser
 
 
 def index(request):
     return HttpResponse('Author working')
 
-# Create your views here.
+
+def publications(request, author_id):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        parser = AuthorParser(author_id=author_id, data_path=tmpdir,date_from=2019, date_to=2021)
+    parser.find_publications()
+    parser.parse_publications()
+
+    return HttpResponse(author_id.publications)
