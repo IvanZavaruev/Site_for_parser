3a42ca014df11cd89c8de7bbf5e63755ddfb9e4b
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-04-09 18:18:16 +0700

    add year parameter to url

--- parser_site/author/views.py
+++ parser_site/author/views.py
@@ -9,9 +9,11 @@ def index(request):
 
 
 def publications(request, author_id):
+    year_from = request.GET.get('year_from', 2015)
+    year_to = request.GET.get('year_to', 2021)
     with tempfile.TemporaryDirectory() as tmpdir:
-        parser = AuthorParser(author_id=author_id, data_path=tmpdir,date_from=2019, date_to=2021)
-    parser.find_publications()
-    parser.parse_publications()
+        parser = AuthorParser(author_id=str(author_id), data_path=tmpdir, date_from=year_from, date_to=year_to)
+        parser.find_publications()
+        parser.parse_publications()
 
     return HttpResponse(author_id.publications)
