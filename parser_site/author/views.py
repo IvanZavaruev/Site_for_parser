961b89e8df3cb4d30976a75ef6739073112861b7
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-04-10 17:42:35 +0700

    add download csv

--- parser_site/author/views.py
+++ parser_site/author/views.py
@@ -11,9 +11,12 @@ def index(request):
 def publications(request, author_id):
     year_from = request.GET.get('year_from', 2015)
     year_to = request.GET.get('year_to', 2021)
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename= author_id + ".csv"'
     with tempfile.TemporaryDirectory() as tmpdir:
-        parser = AuthorParser(author_id=str(author_id), data_path=tmpdir, date_from=year_from, date_to=year_to)
+        parser = AuthorParser(author_id=str(author_id), data_path=tmpdir, date_from=int(year_from), date_to=int(year_to))
         parser.find_publications()
         parser.parse_publications()
+        parser.save_publications(response)
 
-    return HttpResponse(author_id.publications)
+    return response
