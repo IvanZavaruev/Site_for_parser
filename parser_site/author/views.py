d135107edbaa0cd3b8813b8f2091681f1cc8e025
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-04-13 00:11:45 +0700

    fix incorrect of the uploaded file add save_publication code in the correct way

--- parser_site/author/views.py
+++ parser_site/author/views.py
@@ -1,4 +1,6 @@
 import tempfile
+import csv
+from pathlib import Path
 from django.shortcuts import render
 from django.http import HttpResponse
 from elibrary_parser.Parsers import AuthorParser
@@ -12,11 +14,26 @@ def publications(request, author_id):
     year_from = request.GET.get('year_from', 2015)
     year_to = request.GET.get('year_to', 2021)
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename= author_id + ".csv"'
+    response['Content-Disposition'] = f'attachment; filename= "{author_id}.csv"'
     with tempfile.TemporaryDirectory() as tmpdir:
         parser = AuthorParser(author_id=str(author_id), data_path=tmpdir, date_from=int(year_from), date_to=int(year_to))
         parser.find_publications()
         parser.parse_publications()
-        parser.save_publications(response)
+
+        save_path = Path(f"{tmpdir}/processed/{str(author_id)}")
+        save_path.mkdir(exist_ok=True)
+
+        csv_path = save_path / "publications.csv"
+        with open(csv_path, 'a', encoding="utf8", newline='') as csvfile:
+            wr = csv.writer(response, csvfile, delimiter=';')
+            for publication in parser.publications:
+                saving_publication = [
+                    publication.title,
+                    publication.authors,
+                    publication.info,
+                    publication.link,
+                    publication.year
+                ]
+                wr.writerow(saving_publication)
 
     return response
