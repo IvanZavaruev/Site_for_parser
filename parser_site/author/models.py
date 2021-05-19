2fcee71070c73f7f914f05edf0d3ddf664800da2
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-05-19 17:32:22 +0700

    add author table and Years table

--- parser_site/author/models.py
+++ parser_site/author/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=122)
+
+
+class Years(models.Model):
+    years = models.CharField(max_length=20)
+
+
 class Publication(models.Model):
     title = models.CharField(max_length=500)
-    authors = models.CharField(max_length=500)
+    authors = models.ManyToManyField(Author)
     info = models.CharField(max_length=500)
     link = models.CharField(max_length=500)
-    year = models.CharField(max_length=500)
+    year = models.ForeignKey(Years, on_delete=models.CASCADE())
 # Создать класс Автор с соответствующими атрибутами (название, авторы, инфо, ссылки, год )?
 # в урлах ссылка на id автора, а views функция для парсинга
