afd02dce763f83457c372e6d91574ac36dc4b0be
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-05-24 17:22:10 +0700

    added models Author, Years, Publication to admin

--- parser_site/author/admin.py
+++ parser_site/author/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Author, Years, Publication
+
+admin.site.register(Author)
+admin.site.register(Years)
+admin.site.register(Publication)
 # Register your models here.
