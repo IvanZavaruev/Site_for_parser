07a90247a1c5685abad3edbfd279f8af14fb5c6e
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-05-24 16:14:00 +0700

    Added postgresDB

--- parser_site/parser_site/settings.py
+++ parser_site/parser_site/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'parser_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DBforParser',
+        'USER' : f'{Secrets.user}',
+        'PASSWORD' : f'{Secrets.password}',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
