cd9f8752be45c5da2bf8ba31137007452d52b147
IvanZavaruev <ivan.zavaruev@mail.ru>
2021-05-25 17:40:43 +0700

    made code more readable: added functions for csv and json output

--- parser_site/author/views.py
+++ parser_site/author/views.py
@@ -10,45 +10,61 @@ def index(request):
     return HttpResponse('Author working')
 
 
+def return_csv(tmpdir, author_id, response, parser):
+    save_path = Path(f"{tmpdir}/processed/{str(author_id)}")
+    save_path.mkdir(exist_ok=True)
+
+    csv_path = save_path / "publications.csv"
+    with open(csv_path, 'a', encoding="utf8", newline='') as csvfile:
+        wr = csv.writer(response, csvfile, delimiter=';')
+        for publication in parser.publications:
+            saving_publication = [
+                publication.title,
+                publication.authors,
+                publication.info,
+                publication.link,
+                publication.year
+            ]
+            wr.writerow(saving_publication)
+
+    return response
+
+
+def return_json(parser):
+    json_data = []
+    for publication in parser.publications:
+        saving_publication = (
+            publication.title,
+            publication.authors,
+            publication.info,
+            publication.link,
+            publication.year
+        )
+        json_data.append(saving_publication)
+    to_json = {"publications": json_data}
+
+    return to_json
+
+
 def publications(request, author_id):
     format = request.GET.get('format', None)
+
     year_from = request.GET.get('year_from', 2015)
     year_to = request.GET.get('year_to', 2021)
+
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = f'attachment; filename= "{author_id}.csv"'
+
     with tempfile.TemporaryDirectory() as tmpdir:
-        parser = AuthorParser(author_id=str(author_id), data_path=tmpdir, date_from=int(year_from), date_to=int(year_to))
+        parser = AuthorParser(
+            author_id=str(author_id),
+            data_path=tmpdir,
+            date_from=int(year_from),
+            date_to=int(year_to))
         parser.find_publications()
         parser.parse_publications()
 
         if format == 'json':
-            json_data = []
-            for publication in parser.publications:
-                saving_publication = (
-                    publication.title,
-                    publication.authors,
-                    publication.info,
-                    publication.link,
-                    publication.year
-                )
-                json_data.append(saving_publication)
-            to_json = {"publications": json_data}
-            return JsonResponse(to_json)
+            return JsonResponse(return_json(parser))
         else:
-            save_path = Path(f"{tmpdir}/processed/{str(author_id)}")
-            save_path.mkdir(exist_ok=True)
-
-            csv_path = save_path / "publications.csv"
-            with open(csv_path, 'a', encoding="utf8", newline='') as csvfile:
-                wr = csv.writer(response, csvfile, delimiter=';')
-                for publication in parser.publications:
-                    saving_publication = [
-                        publication.title,
-                        publication.authors,
-                        publication.info,
-                        publication.link,
-                        publication.year
-                    ]
-                    wr.writerow(saving_publication)
-
-            return response
+            return return_csv(tmpdir, author_id, response, parser)
